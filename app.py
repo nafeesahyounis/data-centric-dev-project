1e5ecbe5e8c4ac5f99303ec913137d1143bc2eb4
nafeesahyounis <nafeesah.youniss@gmail.com>
2020-06-15 12:06:33 +0000

    working on edit activity function. currently route has been added to edit activity page.

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 from os import path
 if path.exists("env.py"):
   import env 
@@ -10,7 +11,10 @@ app = Flask(__name__)
 app.config["MONGO_DBNAME"] = 'solo_traveller_handbook'
 app.config["MONGO_URI"] = os.environ.get('MONGO_URI')
 
+
+
 mongo = PyMongo(app)
+
 
 @app.route('/')
 @app.route('/home')
@@ -34,6 +38,12 @@ def insert_activity():
     things_to_do = mongo.db.things_to_do
     things_to_do.insert_one(request.form.to_dict())
     return redirect(url_for('index'))
+
+@app.route('/editactivity/<things_to_do_id>')
+
+def edit():
+        the_activity= mongo.db.things_to_do.find_one({"_id":ObjectId(thing_to_do_id)})
+        return render_template("pages/editactivity.html", activity=the_activity)
 
 
 
