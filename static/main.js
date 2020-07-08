19e5a3eaf018b070496469ed737c49f5c183d5d4
nafeesahyounis <nafeesah.youniss@gmail.com>
2020-07-08 21:03:09 +0000

    modal added to delete button to ask user if he/she wants to delete listing before it is deleted

--- static/main.js
+++ static/main.js
@@ -1,1 +1,10 @@
-var input = document.getElementById("")
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.modal');
+    var instances = M.Modal.init(elems, options);
+  });
+
+  // Or with jQuery
+
+  $(document).ready(function(){
+    $('.modal').modal();
+  });
