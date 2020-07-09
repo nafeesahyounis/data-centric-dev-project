ac5db6e26947e03fa7acbba8c58a8e26f90785a2
nafeesahyounis <nafeesah.youniss@gmail.com>
2020-07-09 15:15:06 +0000

    fixed delete modal. wasnt working

--- static/main.js
+++ static/main.js
@@ -1,8 +1,7 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.modal');
-    var instances = M.Modal.init(elems, options);
+$(document).ready(function(){
+    $('.modal').modal();
   });
-
+          
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('select');
     var instances = M.FormSelect.init(elems, options);
