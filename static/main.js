2d53ddbf116a3901c9de0e6779ddbe0d3817b3c4
nafeesahyounis <nafeesah.youniss@gmail.com>
2020-07-11 11:26:39 +0000

    formatted css with css beautifier

--- static/main.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-    $('.modal').modal();
-  });
-          
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('select');
-    var instances = M.FormSelect.init(elems, options);
-});
